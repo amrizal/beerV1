62766ba303910b298954c8c2da14e4c21fdfd08b
Amrizal <amrizal@outlook.com>
2015-09-04 10:59:43 +0800

    Page 90

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -16,14 +16,14 @@ public class BeerSelect extends HttpServlet
 			BeerExpert be = new BeerExpert();
 			List result = be.getBrands(c);
 			
-			response.setContentType("text/html");
-			PrintWriter out = response.getWriter();
-			out.println("Beer Selection Advice<br>");
+			//response.setContentType("text/html");
+			//PrintWriter out = response.getWriter();
+			//out.println("Beer Selection Advice<br>");
 			
-			Iterator it = result.iterator();
-			while(it.hasNext())
-			{
-				out.println("<br>try: " + it.next());
-			}
+			request.setAttribute("styles", result);
+			
+			RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+			
+			view.forward(request, response);
 		}
 }
--- web/result.jsp
+++ web/result.jsp
@@ -1,11 +1,11 @@
-<% page import="java.util.*" %>
+<%@ page import="java.util.*" %>
 <html>
 <body>
 <h1 align="center">Beer Recommendations JSP</h1>
 <p>
 
 <%
-	List styles = (List)request.getAttribute("style");
+	List styles = (List)request.getAttribute("styles");
 	Iterator it = styles.iterator();
 	while(it.hasNext())
 	{
